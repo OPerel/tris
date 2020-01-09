4c68c74beff03cd0f1c0feb8c7e41825fd86b73c
Operel <oriperelman@gmail.com>
2020-01-09 21:49:49 +0200

    Initial commit - basic layers prrallax

---
+++ src/index.js
@@ -0,0 +1,46 @@
+// import Parallax from 'parallax-js';
+import './styles.scss';
+
+import _0 from './assets/0.png';
+import _1 from './assets/1.png';
+import _2 from './assets/2.png';
+import _3 from './assets/3.png';
+import _4 from './assets/4.png';
+import _5 from './assets/5.png';
+
+
+
+
+
+// const container = document.getElementsByClassName('container')[0];
+// const scrollX = document.getElementsByClassName('scroll-x')[0];
+// const projects = document.getElementsByClassName('projects')[0]
+// const contact = document.getElementsByClassName('contact')[0]
+
+// window.addEventListener('wheel', function(e) {
+//   let scrollXScrolled = Math.ceil(scrollX.scrollWidth - scrollX.scrollLeft) === scrollX.clientWidth;
+//   let containerScrolled = Math.ceil(container.scrollHeight - container.scrollTop) === container.clientHeight;
+//   // const contactScrolled = contact.scrollTop === (contact.scrollHeight - contact.offsetHeight);
+  
+//   console.log('containerScrolled: ', containerScrolled);
+//   console.log('scrollHeight: ', container.scrollHeight, 'scrollTop: ', container.scrollTop, 'clientHeight: ', container.clientHeight)
+//   console.log('scrollXScrolled: ', scrollXScrolled);
+//   console.log('scrollHeight: ', scrollX.scrollWidth, 'scrollTop: ', scrollX.scrollLeft, 'clientHeight: ', scrollX.clientWidth)
+
+//   if (containerScrolled) {
+//     e.preventDefault();
+//     if (e.deltaY > 0) {
+//       scrollX.scrollLeft += 100;
+//     }
+//     else {
+//       scrollX.scrollLeft -= 100;
+//     }
+//     if (!scrollXScrolled) {
+//       console.log('set containerScrolled to false')
+//       containerScrolled = false;
+//     }
+//   }
+// }, { passive: false });
+
+
+    
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jpg|png)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: "[name].[ext]",
+              outputPath: "assets",
+              esModule: false
+            }
+          }
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ],
+      },
+    ],
+  }
+};
