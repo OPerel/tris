c38de8c95702ae3d83dc3064c4d6f8f0c21b3f6c
Operel <oriperelman@gmail.com>
2020-01-13 22:07:30 +0200

    a lot of changes... all animations are now based on css scrolling var from js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,77 @@
 // import Parallax from 'parallax-js';
 import './styles.scss';
 
-import _0 from './assets/0.png';
-import _1 from './assets/1.png';
-import _2 from './assets/2.png';
-import _3 from './assets/3.png';
-import _4 from './assets/4.png';
-import _5 from './assets/5.png';
+// import _0 from './assets/0.png';
+// import _1 from './assets/1.png';
+// import _2 from './assets/2.png';
+// import _3 from './assets/3.png';
+// import _4 from './assets/4.png';
+// import _5 from './assets/5.png';
 
+// const tris = document.getElementsByClassName('tris')[0];
+// const square = document.getElementsByClassName('square')[0];
+const gallery = document.getElementsByClassName('gallery')[0];
+// const navItems = document.querySelectorAll('li');
+const container = document.getElementsByClassName('container')[0];
+// const about = document.getElementById('about');
+// const projects = document.getElementById('projects');
+// const contact = document.getElementById('contact');
+
+// The debounce function receives our function as a parameter
+const debounce = (fn) => {
+  console.log('debounce is running')
+  // This holds the requestAnimationFrame reference, so we can cancel it if we wish
+  let frame;
+
+  // The debounce function returns a new function that can receive a variable number of arguments
+  return (...params) => {
+    
+    // If the frame variable has been defined, clear it now, and queue for next frame
+    if (frame) { 
+      cancelAnimationFrame(frame);
+    }
+
+    // Queue our function call for the next frame
+    frame = requestAnimationFrame(() => {
+      
+      // Call our function and pass any params we received
+      fn(...params);
+    });
+
+  } 
+};
+
+
+// Reads out the scroll position and stores it in the data attribute
+// so we can use it in our stylesheets
+const storeScroll = () => {
+  // const containerHeight = container.clientHeight * 4;
+  // console.log(window.scrollY, document.body.clientHeight, window.innerHeight)
+  let scrollpos = `${Math.floor(window.scrollY / (document.body.clientHeight - window.innerHeight) * 350)}`;
+  document.documentElement.setAttribute('style', `--scrollpos: ${scrollpos}`);
+}
+
+// Listen for new scroll events, here we debounce our `storeScroll` function
+document.addEventListener('scroll', debounce(storeScroll), { passive: true });
+
+// Update scroll position for first time
+storeScroll();
+
+gallery.addEventListener('wheel', (e) => {
+  e.preventDefault();
+  if (e.deltaY > 0) {
+    gallery.scrollLeft += 100;
+  }
+  else {
+    gallery.scrollLeft -= 100;
+  }
+})
+
+// for (let i = 0; 0 < 4; i++) {
+//   const tri = document.createElement('div');
+//   tri.classList.add('tris', `tris_${i}`)
+//   container.appendChild(tri);
+// }
 
 
 
